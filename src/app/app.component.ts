da35892efd680d87654c835d081580b94acf050a
NeckitWin <neckit.v@gmail.com>
2023-10-22 02:18:50 +0200

    upload new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,40 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  personalInfo = {
-    name: 'Mykyta Vinohradov',
-    photo: 'https://avatars.githubusercontent.com/u/90260450?v=4',
-    email: 'neckit.v@gmail.com',
-    github: 'NeckitWin',
-  };
+  profileImageSrc = 'https://avatars.githubusercontent.com/u/90260450?v=4';
+  profileName = 'NecktiWin';
+  isPro = true;
 
-  // другие разделы...
+  phoneNumber = '735-580-571';
+  country = 'Poland';
+  email = 'neckit.v@gmail.com';
+  education = 'Technikum Nr I ZSTI';
+
+  // Другие переменные и данные
+
+  // Обработчик для позвонить по номеру телефона
+  callPhoneNumber() {
+    window.location.href = 'tel:' + this.phoneNumber;
+  }
+
+  // Обработчик для отправки письма на адрес электронной почты
+  sendEmail() {
+    window.location.href = 'mailto:' + this.email;
+  }
+
+  aboutMeText = "Hello! 👋 I'm Nikita. What if I told you that everything you see isn't real? It's all just code. 💡";
+
+  whatIDoText = "I am a Full-stack developer with a passion for programming, particularly my favorite language, Java. I also specialize in web development, utilizing the following languages:";
+
+  skills = [
+    '⭐️⭐️ Java, my beloved programming language.',
+    '⭐️⭐️⭐️⭐️⭐️ HTML for content structuring.',
+    '⭐️⭐️⭐️⭐️⭐️ CSS for style and elegance.',
+    '⭐️⭐️⭐️ JavaScript for interactivity.',
+    '⭐️⭐️ Node.js for server-side magic.'
+  ];
+
+  connectWithMeText = "If you want to chat about projects, ideas, or just have a friendly conversation, reach out to me on:";
+  socialMedia = ['Discord', 'Instagram'];
+  moonText = "What if you couldn't wake up? 🌙";
 }
