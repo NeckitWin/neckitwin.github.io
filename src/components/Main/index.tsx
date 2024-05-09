a2d6cc451868ccc37d987e678f83f90cab6224bf
NeckitWin <neckit.v@gmail.com>
2024-05-09 20:14:40 +0200

    add my experience

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,9 @@
 import s from './Main.module.css';
 import {useTranslation} from "react-i18next";
-import plangs from './plangs.json';
+import plangs from './Data/plangs.json';
+import libs from './Data/libs.json';
+import engines from './Data/engines.json';
+import tools from './Data/tools.json';
 
 const Main = () => {
     const {t} = useTranslation();
@@ -13,9 +16,28 @@ const Main = () => {
                     <p>{t("name")}</p>
                     <p>{t("description")}</p>
                 </section>
-                <section>
+                <section className={s.exp}>
+                    <h3>{t("languages")}</h3>
                     <ul className={s.plangs}>
                         {plangs.map(el => (
+                            <li style={{backgroundColor: el.color}}><img src={el.icon} alt={el.name}/>{el.name}</li>
+                        ))}
+                    </ul>
+                    <h3>{t("libs")}</h3>
+                    <ul className={s.plangs}>
+                        {libs.map(el => (
+                            <li style={{backgroundColor: el.color}}><img src={el.icon} alt={el.name}/>{el.name}</li>
+                        ))}
+                    </ul>
+                    <h3>{t("engines")}</h3>
+                    <ul className={s.plangs}>
+                        {engines.map(el => (
+                            <li style={{backgroundColor: el.color}}><img src={el.icon} alt={el.name}/>{el.name}</li>
+                        ))}
+                    </ul>
+                    <h3>{t("tools")}</h3>
+                    <ul className={s.plangs}>
+                        {tools.map(el => (
                             <li style={{backgroundColor: el.color}}><img src={el.icon} alt={el.name}/>{el.name}</li>
                         ))}
                     </ul>
