fdce35ba5a0127d0ff31d69afc18168d62b25a45
NeckitWin <neckit.v@gmail.com>
2024-12-15 16:42:40 +0100

    add sections about me

--- src/components/CustomButtons.tsx
+++ src/components/CustomButtons.tsx
@@ -7,9 +7,9 @@ const CustomButtons: React.FC<{ links: Links[] }> = ({links}) => {
     return (
         <ul className='flex flex-row gap-6'>
             {links.map((link) => (
-            <li key={link.name} className='flex items-center transition hover:-translate-y-0.5'>
-                <a href={link.link}
-                   className='flex flex-row gap-1.5 text-white px-2 py-1.5 rounded-lg shadow-md bg-neutral-900/50 shadow-neutral-900'>
+            <li key={link.name} className='transition hover:-translate-y-0.5'>
+                <a href={link.link} rel='noreferrer' target='_blank'
+                   className='flex flex-row gap-1.5 text-white px-2 py-1.5 rounded-lg shadow-md bg-neutral-900/50 shadow-neutral-900/90'>
                     {link.name}
                 </a>
             </li>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,6 @@
 import Section from "../components/Section.tsx";
 import CustomButtons from "../components/CustomButtons.tsx";
+import {useState} from "react";
 
 const animaLinks = [
     {name: 'Invite', link: 'https://discord.com/oauth2/authorize?client_id=1187466797885182141'},
@@ -8,20 +9,24 @@ const animaLinks = [
 ];
 
 function Main() {
+    const [activeSection, setActiveSection] = useState<number>(1)
+
     return (
         <main>
             <section className='my-10 mx-2 text-textColor flex flex-col gap-2'>
                 <div className='flex flex-row'>
                     <h1 className='text-2xl font-medium'>Hi, Im Nikita</h1>
                     <span
-                        className='bg-gradient-to-r from-purple-500 to-pink-500 text-white rounded-full px-2 py-1 flex items-center mx-2 mb-2.5 text-xs'>Fullstack</span>
+                        className='bg-gradient-to-r from-purple-500 to-pink-500 text-white rounded-full px-2 py-1 flex items-center mx-2 mb-2.5 text-xs'>
+                        Programmer
+                    </span>
                 </div>
                 <p>Im a fullstack developer, I love to code and learn new things. I have experience in web development
                     and I am currently learning mobile development.</p>
             </section>
             <Section caption={'Projects'}/>
             <section
-                className='my-10 mx-2 text-textColor p-6 rounded-2xl shadow-md bg-gradient-to-br from-pink-700 to-amber-700 shadow-neutral-900'>
+                className='my-10 mx-2 text-textColor p-6 rounded-2xl shadow-md bg-gradient-to-br from-pink-600 to-amber-600 shadow-neutral-900'>
                 <div className='flex flex-row gap-5 mb-5'>
                     <img src="/anima.png" alt="Anima" className='max-w-16 max-h-16 rounded-full'/>
                     <div>
@@ -35,8 +40,23 @@ function Main() {
                 <CustomButtons links={animaLinks}/>
             </section>
             <Section caption={'Skills'}/>
-            <section>
-
+            <section className='my-6 mx-2 text-textColor flex flex-col gap-2.5'>
+                <nav className='flex flex-row gap-5'>
+                    <button className=''
+                        onClick={() => setActiveSection(1)}>Language</button>
+                    <button className=''
+                        onClick={() => setActiveSection(2)}>Technology</button>
+                </nav>
+                {activeSection == 1 &&
+                    <section className={activeSection == 1 ? 'visible' : 'hidden'}>
+                        language
+                    </section>
+                }
+                {activeSection == 2 &&
+                    <section className={activeSection == 2 ? 'visible' : 'hidden'}>
+                        technology
+                    </section>
+                }
             </section>
         </main>
     )
