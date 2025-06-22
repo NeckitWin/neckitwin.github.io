d286622855e7ea3b46e43c8fc908e6ae9065a1fb
NeckitWin <neckit.v@gmail.com>
2025-06-22 13:58:21 +0200

    empty react ts project

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import Header from "./components/Header.tsx";
-import Main from "./components/Main.tsx";
-import Footer from "./components/Footer.tsx";
-import ParticlesBackground from "./components/ParticlesBackground.tsx";
+const App = () => {
 
-function App() {
-
-
-    return (
-        <>
-            <div className='container mx-auto max-w-5xl p-6 relative z-10'>
-                <Header/>
-                <Main/>
-                <Footer />
-            </div>
-            <ParticlesBackground />
-        </>
-    )
+  return (
+    <>
+    
+    </>
+  )
 }
 
-export default App;
+export default App
