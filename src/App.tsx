e0da9d3d30ddd7aace7b2c30cd7d896684cd0afd
NeckitWin <neckit.v@gmail.com>
2025-10-10 23:26:03 +0200

    create react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import Header from "./components/Header.tsx";
-import Main from "./components/Main.tsx";
-import Footer from "./components/Footer.tsx";
-import ParticlesBackground from "./components/ParticlesBackground.tsx";
-
-function App() {
-
-
-    return (
-        <>
-            <div className='container mx-auto max-w-5xl p-6 relative z-10'>
-                <Header/>
-                <Main/>
-                <Footer />
-            </div>
-            <ParticlesBackground />
-        </>
-    )
+const App = () => {
+  return (
+    <></>
+  )
 }
 
-export default App;
+export default App
--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className='py-4 px-5 rounded-xl shadow-md shadow-bgColor bg-[#1F1F1F]'>
-            <h3 className='text-textColor text-center'>&copy; 2025 NeckitWin</h3>
-        </footer>
-    )
-}
-
-export default Footer;
