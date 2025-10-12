8cde359938773d340c1f9d40416678420d35beec
NeckitWin <neckit.v@gmail.com>
2025-10-12 23:30:44 +0200

    feat: footer content and contact links

--- src/modules/footer.tsx
+++ src/modules/footer.tsx
@@ -1,7 +1,24 @@
+import { links } from "../lib/constants/links";
+import ContactButton from "../ui/contactButton";
+import GithubIcon from "../ui/icons/githubIcon";
+import LinkedinIcon from "../ui/icons/linkedinIcon";
+
+const contactLinks = [
+    { href: links.github, icon: <GithubIcon className="w-6 h-6 " /> },
+    { href: links.linkedin, icon: <LinkedinIcon className="w-6 h-6 " /> }
+]
+
 const Footer = () => {
     return (
-        <footer>
-            <span>&copy; 2025 / by NeckitWin</span>
+        <footer className="w-full text-white dark:text-black flex flex-row justify-around items-center py-4 px-12">
+            <span className="text-sm text-neutral-300">&copy;2025 by NeckitWin</span>
+            <ul className="flex flex-row gap-6">
+                {contactLinks.map((link, index) => (
+                    <li key={index}>
+                        <ContactButton link={link.href}>{link.icon}</ContactButton>
+                    </li>
+                ))}
+            </ul>
         </footer>
     )
 }
