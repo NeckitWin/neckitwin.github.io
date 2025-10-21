90cd84be8572b6fafbc0a584e7bb4d85d37faf7f
NeckitWin <neckit.v@gmail.com>
2025-10-21 23:16:08 +0200

    style: header responsiveness

--- src/modules/header.tsx
+++ src/modules/header.tsx
@@ -12,8 +12,8 @@ const routes = [
 
 const Header = () => {
     return (
-        <header className="w-full text-white dark:text-black flex flex-row justify-between items-center sticky py-4 px-12">
-            <span className="w-48"><img src="neckitwin.png" alt="logo" className="h-10 w-10" /></span>
+        <header className="w-full text-white dark:text-black flex flex-row md:justify-between justify-center items-center md:sticky py-4 md:px-12">
+            <span className="w-48 hidden md:block"><img src="neckitwin.png" alt="logo" className="h-10 w-10" /></span>
             <nav className="bg-black/80 dark:bg-white/80 border border-white/20 dark:border-black/20 backdrop-blur-md rounded-2xl py-0.5 px-1 shadow-lg flex">
                 <Link to='/' className="py-2 px-4 cursor-pointer rounded-xl dark:hover:bg-black/5 hover:bg-white/20 duration-200">
                     <ProfileIcon className="h-4 w-4 fill-white dark:fill-black" />
@@ -25,7 +25,7 @@ const Header = () => {
                 </ul>
                 <div className="flex items-center py-1.5 px-4 rounded-xl dark:hover:bg-black/5 hover:bg-white/20 cursor-pointer duration-200"><DarkModeButton /></div>
             </nav>
-            <span className="font-medium w-48 text-end">{content.header.location}</span>
+            <span className="font-medium w-48 text-end hidden md:block">{content.header.location}</span>
         </header>
     )
 }
