6fa5aba28d32d7ed309e3bbaed1ca9cd53bbf64f
NeckitWin <neckit.v@gmail.com>
2025-10-21 23:25:27 +0200

    style: responsiveness of homepage

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -6,11 +6,11 @@ import Avatar from "../ui/avatar";
 const Home = () => {
     return (
         <CenterBox>
-            <section className="flex flex-row items-center gap-12">
+            <section className="flex md:flex-row flex-col items-center gap-12 flex-wrap justify-center">
                 <Avatar size={200} decorationSize={242} src={links.avatar} decoration={links.decoration} />
-                <div className="flex flex-col gap-4">
-                    <div className="flex flex-col items-start gap-2">
-                        <h2 className="text-7xl font-medium text-white dark:text-black">{content.home.owner}</h2>
+                <div className="flex flex-col gap-4 md:items-start items-center md:px-0 px-4">
+                    <div className="flex md:flex-col md:items-start gap-2 flex-row items-center">
+                        <h2 className="md:text-7xl text-[40px] font-medium text-white dark:text-black">{content.home.owner}</h2>
                         <span className="bg-gradient-to-br from-red-200 to-rose-950 text-white px-2 py-0.5 rounded-lg font-medium">{content.home.role}</span>
                     </div>
                     <p className="text-white dark:text-black text-lg">{content.home.description}</p>
